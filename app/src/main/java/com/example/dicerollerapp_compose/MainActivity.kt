cd22ad6875ecf6ac41e2d909d03fc47166a1bf20
Windson Viana <windson@virtual.ufc.br>
2024-12-04 14:17:42 -0300

    Mudanças no Layout em Compose

--- app/src/main/java/com/example/dicerollerapp_compose/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp_compose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +13,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -32,8 +35,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DiceRollerApp_ComposeTheme {
+            DiceRollerApp_ComposeTheme (dynamicColor = false) {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                   // modifier = Modifier.wrapContentSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     DiceRollerApp()
+                }
+
                 }
             }
         }
@@ -44,24 +55,20 @@ class MainActivity : ComponentActivity() {
 fun DiceRollerApp() {
 
     DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
+       // .fillMaxSize()
         .wrapContentSize(Alignment.Center))
 }
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     var result by remember { mutableStateOf(1) }
-    var imageResource = when (result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
+    var imageResource  by remember { mutableStateOf(R.drawable.dice_6)}
+
+    modifier.background(MaterialTheme.colorScheme.background)
     Column (
         modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+       horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
         Image(
             painter = painterResource(imageResource),
--- app/src/main/java/com/example/dicerollerapp_compose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/dicerollerapp_compose/ui/theme/Theme.kt
@@ -9,17 +9,21 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
+    background = Color (0xFF301934),
     tertiary = Pink80
+
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
+    //background = Color.Green,
     tertiary = Pink40
 
     /* Other default colors to override
